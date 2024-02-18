b2bc343af1f1b9049e5daf09f22eb724e4920618
akshay malviya <102719327+akshay-malviya12@users.noreply.github.com>
2024-02-18 20:25:00 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,27 @@
+{
+  "name": "nodejs",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "@types/node": "^18.0.6",
+    "bcrypt": "^5.1.1",
+    "bull": "^4.12.2",
+    "bull-arena": "^4.2.0",
+    "bull-board": "^2.1.3",
+    "express": "^4.18.2",
+    "express-graphql": "^0.12.0",
+    "graphql": "^16.8.1",
+    "jsonwebtoken": "^9.0.2",
+    "node-fetch": "^3.2.6",
+    "nodemailer": "^6.9.9",
+    "redis": "^4.6.13",
+    "sqlite3": "^5.1.7"
+  }
+}
